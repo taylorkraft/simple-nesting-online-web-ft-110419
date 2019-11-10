75aa279162f0e491f6d8d413fee78febffdd2f87
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-10 16:14:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,12 +63,6 @@ programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -109,10 +103,10 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      
+programmer_hash     
 end
 
 def adding_to_dennis
