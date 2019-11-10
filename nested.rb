1d099032bd6e3d5f5981ae742c64a765b3b8363e
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-10 15:48:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -17,11 +17,12 @@ def hopper
      }
 
 programmer_hash[:grace_hopper]
+
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	programmer_hash[:grace_hopper][:known_for]
+	
+	programmer_hash[:alan_kay][:known_for]
 	
 	programmer_hash = 
  		{
