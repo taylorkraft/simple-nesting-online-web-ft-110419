b25c3b222f548d3222cee0c6839007936cc6a905
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-10 15:48:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -22,8 +22,6 @@ end
 
 def alan_kay_is_known_for
 	
-	programmer_hash[:alan_kay][:known_for]
-	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -39,6 +37,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
